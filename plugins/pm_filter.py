dff6f994756a7109f23927088ecd493168a82443
Autobot09 <123814844+Autobot09@users.noreply.github.com>
2023-01-28 22:19:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -725,7 +725,7 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text="ꜱᴇʟᴇᴄᴛ ꜰɪʟᴇ ꜰʀᴏᴍ ᴀʙᴏᴠᴇ ʟɪɴᴋꜱ", callback_data="pages")]
         )
-    imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
+    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     TEMPLATE = settings['template']
     if imdb:
         cap = TEMPLATE.format(
